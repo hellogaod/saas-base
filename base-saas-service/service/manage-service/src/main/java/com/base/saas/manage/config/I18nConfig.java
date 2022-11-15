4f1e5bccadc75cb3581b5edc5da00f726ca79527
9527future <1950928205@qq.com>
2022-11-15 14:31:52 +0800

    国际化语言配置完成

--- base-saas-service/service/manage-service/src/main/java/com/base/saas/manage/config/I18nConfig.java
+++ base-saas-service/service/manage-service/src/main/java/com/base/saas/manage/config/I18nConfig.java
@@ -2,8 +2,10 @@ package com.base.saas.manage.config;
 
 import com.base.saas.util.redis.RedisKeyConstants;
 import com.base.saas.util.redis.RedisUtil;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -14,16 +16,25 @@ import java.util.Locale;
 
 /**
  * @ClassName I18nConfig
- * @Description 多语言初始化文件
+ * @Description 配置国际化语言
  */
 @Configuration
 public class I18nConfig extends WebMvcConfigurerAdapter {
+
+    @Bean(name = "messageSource")
+    public ResourceBundleMessageSource getMessageSource() throws Exception {
+        ResourceBundleMessageSource resourceBundleMessageSource = new ResourceBundleMessageSource();
+        resourceBundleMessageSource.setDefaultEncoding("UTF-8");
+        resourceBundleMessageSource.setBasenames("i18n/messages");
+        return resourceBundleMessageSource;
+    }
+
     @Bean
     public LocaleResolver localeResolver() {
         CookieLocaleResolver slr = new CookieLocaleResolver();
         slr.setDefaultLocale(Locale.SIMPLIFIED_CHINESE);
         Locale locale = new Locale("zh", "CN");
-        RedisUtil.set(RedisKeyConstants.SYS_LANGUAGE,locale);
+        RedisUtil.set(RedisKeyConstants.SYS_LANGUAGE, locale);
         return slr;
     }
 
@@ -33,7 +44,6 @@ public class I18nConfig extends WebMvcConfigurerAdapter {
         lci.setParamName("langType");
         return lci;
     }
-
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(localeChangeInterceptor());
